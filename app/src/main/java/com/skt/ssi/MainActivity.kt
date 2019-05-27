4f9788fc638e5b9def705e67a3e39e8695cf8650
babo <seirion@mensakorea.org>
2019-05-27 20:08:48 +0900

    remove test code
    only need to initialize LibIndy

--- app/src/main/java/com/skt/ssi/MainActivity.kt
+++ app/src/main/java/com/skt/ssi/MainActivity.kt
@@ -1,57 +1,24 @@
 package com.skt.ssi
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.system.ErrnoException
-import android.system.Os
 import android.util.Log
 import org.hyperledger.indy.sdk.LibIndy
-import org.hyperledger.indy.sdk.wallet.Wallet
-import org.json.JSONObject
-import java.util.concurrent.ExecutionException
 
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dataDir = applicationContext.dataDir
-        System.out.println("datadir= ${dataDir.absolutePath}")
-        val externalFilesDir = getExternalFilesDir(null)
-        val path = externalFilesDir!!.absolutePath
-        println("axel externalFilesDir=$path")
-
-        try {
-            Os.setenv("EXTERNAL_STORAGE", path, true)
-        } catch (e: ErrnoException) {
-            e.printStackTrace()
-        }
-
+        Log.d(TAG, "init lib-indy")
         LibIndy.init()
-        val WALLET = "Wallet1"
-        val TYPE = "default"
-        val WALLET_CREDENTIALS = JSONObject()
-            .put("key", "key")
-            .toString()
-        val WALLET_CONFIG = JSONObject()
-            .put("id", WALLET)
-            .put("storage_type", TYPE)
-            .toString()
+    }
 
-        try {
-            Wallet.createWallet(WALLET_CONFIG, WALLET_CREDENTIALS).get()
-        } catch (e: ExecutionException) {
-            println(e.message)
-            if (e.message!!.indexOf("WalletExistsException") >= 0) {
-                // ignore
-            } else {
-                throw RuntimeException(e)
-            }
-        }
-
-        val wallet = Wallet.openWallet(WALLET_CONFIG, WALLET_CREDENTIALS).get()
-        Log.d("TEST", "wallet: $wallet")
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
     }
 }
